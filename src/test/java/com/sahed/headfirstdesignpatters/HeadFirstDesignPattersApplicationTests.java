e0178602716cd2c6ac8b0c2f765f2fd0a9db1530
Sahed <sahed.moral@123rf.com>
2023-12-12 15:22:06 +0600

    init

---
+++ src/test/java/com/sahed/headfirstdesignpatters/HeadFirstDesignPattersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sahed.headfirstdesignpatters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeadFirstDesignPattersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
